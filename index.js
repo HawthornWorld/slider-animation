967c73caffd6a287aaf47777881892b61ad9cb54
mosic <mosic.ma@hotmail.com>
2017-07-05 13:12:29 +0800

    slider

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by yanma on 17-7-5.
+ */
+var $ = function(id) {
+    return document.getElementById(id);
+};
+var $box = $("transImageBox"),
+    $oTrigger = $("transImageTrigger").getElementsByTagName("a"),
+    $len = $oTrigger.length;
+for(var i = 0; i < $len; i++) {
+    $oTrigger[i].onclick = function(){
+        var index = Number(this.href.replace(/.*#/g,'')) || 1;
+        $box.style.marginLeft = (1 - index) * 300 + "px";
+        return false;
+    };
+}
