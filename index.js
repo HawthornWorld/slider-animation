bfa0fa14db58c5742f0c410bc3472d1a3fa8c96a
马燕 <mosic.ma@hotmail.com>
2017-07-12 16:39:25 +0800

    add explain

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 /**
  * Created by yanma on 17-7-5.
  */
@@ -9,8 +10,8 @@ var $box = $("transImageBox"),
     $len = $oTrigger.length;
 for(var i = 0; i < $len; i++) {
     $oTrigger[i].onclick = function(){
-        var index = Number(this.href.replace(/.*#/g,'')) || 1;
-        $box.style.marginLeft = (1 - index) * 300 + "px";
+        var index = Number(this.href.replace(/.*#/g,'')) || 1;//key steps
+        $box.style.marginLeft = (1 - index) * 300 + "px";//key steps
         return false;
     };
 }
